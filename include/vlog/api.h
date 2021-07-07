49c985d80d86dded0c939246239d58077ae841e8
Larry Gonzalez <larryjgonzalez@gmail.com>
2021-07-07 18:59:20 +0200

    minimal example

---
+++ include/vlog/api.h
@@ -0,0 +1,41 @@
+/*
+ * @author Larry Gonzalez
+ * Based on https://raw.githubusercontent.com/karmaresearch/vlog/master/src/vlog/java/native/VLog.cpp
+ *
+ */
+
+#include <vlog/concepts.h>
+#include <vlog/edb.h>
+#include <vlog/edbconf.h>
+#include <vlog/seminaiver.h>
+#include <vlog/cycles/checker.h>
+#include <vlog/reasoner.h>
+#include <vlog/utils.h>
+#include <kognac/utils.h>
+#include <kognac/logs.h>
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdint>
+#include <vector>
+
+#ifndef VLOGAPI_API_H
+#define VLOGAPI_API_H
+
+class API {
+private:
+    SemiNaiver *sn;
+    Program *program;
+    EDBLayer *layer;
+public:
+    void addRule(std::string rule);
+    void addFacts(std::string predicate, std::vector<std::vector<std::string>> data);
+    void materialize(bool skolem = false, unsigned long timeout = 0);
+    long extensionsize(std::string predicate);
+    API();
+    ~API();
+};
+
+#endif //VLOGAPI_API_H
+
