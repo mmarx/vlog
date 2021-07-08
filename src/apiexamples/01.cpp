f4709e243d2128ff674c33193701e02d25ab3cf5
Larry Gonzalez <larryjgonzalez@gmail.com>
2021-07-08 11:02:35 +0200

    add comments to example; improve code; add api.md

--- src/apiexamples/01.cpp
+++ src/apiexamples/01.cpp
@@ -8,20 +8,26 @@
 #include <iostream>
 
 int main() {
+    // Initialize an API
     API api;
 
-    std::string predicate = "p";
-    std::vector<std::string> row;
-    row.push_back("a");
-    std::vector<std::vector<std::string>> data;
-    data.push_back(row);
-    api.addFacts(predicate, data);
+    // Create a vector of vectors to store a predicate's extension
+    std::vector<std::vector<std::string>> pExtension;
+    // Add some data to it
+    pExtension.push_back({"a"});
+    pExtension.push_back({"b"});
 
+    // Add the predicate name and its extension to the API instance
+    api.addFacts("p", pExtension);
+
+    // Add some rule to the API instance
     api.addRule("q(X) :- p(X)");
     api.addRule("r(X) :- q(X)");
 
+    // Call materialize
     api.materialize();
 
+    // Count extension sizes (Only for IDB predicates)
     std::cout << "q: " << api.extensionsize("q") << std::endl;
     std::cout << "r: " << api.extensionsize("r") << std::endl;
 
